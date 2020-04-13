160f04e5bcf4b8611ec058d64391f552aab4940e
akioxfyz <akioxyz@mail.ru>
2020-04-13 11:21:48 +0200

    Hello World

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeOfMy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo World");
+            Console.ReadLine();
+        }
+    }
+}
